3c306fe16ea0c54900301ca976c37aab36f2621f
Stephen Lindauer <stephen.lindauer@gmail.com>
2012-11-12 19:24:23 -0600

    only show video frames when motion

--- detect.py
+++ detect.py
@@ -8,7 +8,9 @@ import datetime
 import os
 import cv
 
-REQUIRED_DETECTION=400
+REQUIRED_DETECTION=100
+THRESHOLD = 120
+ALWAYS_SHOW_FRAME = False
 
 def my_range(start, end, step):
     while start<=end:
@@ -27,12 +29,17 @@ def pix_change2(p1, p2, x, y):
     b = 0
     return p2[x,y]
 
+def show_frame(frame):
+    
+    cv.ShowImage("Window", frame)
+
+
 frames = []
 recording = False
 inactive_frames = 0
 video_number = 0
 
-capture = cv.CaptureFromCAM(0)
+capture = cv.CaptureFromCAM(1)
 
 last_frame = cv.QueryFrame(capture)
 
@@ -52,17 +59,15 @@ while True:
     pixels1 = last_frame
     pixels2 = display_frame
     
-    
-    thresh = 50
     diff_count = 0
     
     for y in my_range(0,frame.width-1, 4):
         for x in my_range(0, frame.height-1, 4):
-
-            val1 = pixels1[x,y][0] + pixels1[x,y][1] + pixels1[x,y][2]
-            val2 = pixels2[x,y][0] + pixels2[x,y][1] + pixels2[x,y][2]
-
-            if abs(val1-val2) > thresh:
+            red_diff = abs(pixels1[x,y][0] - pixels2[x,y][0])
+            green_diff = abs(pixels1[x,y][0] - pixels2[x,y][0])
+            blue_diff = abs(pixels1[x,y][0] - pixels2[x,y][0])
+            
+            if red_diff + blue_diff + green_diff > THRESHOLD:
                 diff_count += 1
 
                 pixels2[x,y] = pix_change1(pixels1, pixels2, x, y)
@@ -86,13 +91,11 @@ while True:
         
         
         
-    cv.Rectangle(display_frame, (frame.width/2-BAR_WIDTH/2, 690), (frame.width/2+BAR_WIDTH/2, 710), (0,0,0), -1)
-    cv.Rectangle(display_frame, (frame.width/2-BAR_WIDTH/2, 690), (frame.width/2+BAR_WIDTH/2, 710), color)
+    cv.Rectangle(display_frame, (frame.width/2-BAR_WIDTH/2, frame.height-30), (frame.width/2+BAR_WIDTH/2, frame.height-10), (0,0,0), -1)
+    cv.Rectangle(display_frame, (frame.width/2-BAR_WIDTH/2, frame.height-30), (frame.width/2+BAR_WIDTH/2, frame.height-10), color)
 
-    cv.Rectangle(display_frame, (frame.width/2-BAR_WIDTH/2, 690), (int(frame.width/2-BAR_WIDTH/2+BAR_WIDTH*percent), 710), color, -1)
+    cv.Rectangle(display_frame, (frame.width/2-BAR_WIDTH/2, frame.height-30), (int(frame.width/2-BAR_WIDTH/2+BAR_WIDTH*percent), frame.height-10), color, -1)
     
-    cv.ShowImage("Window", display_frame)
-
     last_frame = cv.CreateImage(cv.GetSize(frame), 8, 3)
     cv.Copy(frame, last_frame)
 
@@ -102,13 +105,20 @@ while True:
             recording = True
             video_number += 1
             writer = cv.CreateVideoWriter("out-%s.avi" % (video_number), cv.CV_FOURCC('M', 'J', 'P', 'G'), 5, cv.GetSize(frame), True)
-
+            
+            cv.NamedWindow("Window", flags=cv.CV_WINDOW_NORMAL)
+            cv.MoveWindow("Window", 2500, 20)
+            
         if recording:
             cv.WriteFrame(writer, display_frame)
-            
+        
+        show_frame(display_frame) 
     
     else:
+
         if recording:
+            show_frame(display_frame)
+            
             inactive_frames += 1
             cv.WriteFrame(writer, display_frame)
             
@@ -117,6 +127,8 @@ while True:
                 inactive_frames = 0                
                 print 'Save video'
                 del writer
+                
+                cv.DestroyWindow("Window")
 
 
 
